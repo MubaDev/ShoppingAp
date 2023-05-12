0e05d4266b3960402ce6fe7758773117c4304280
MubaDev <mube5265@gmail.com>
2023-05-12 14:29:57 +0300

    Initial Commit

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
-
+     recipes: Recipe[] = [
+      new Recipe('Recipe Test','Recipe Test Description',
+      'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'),
+      new Recipe('Recipe Test','Recipe Test Description',
+      'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg')
+     ]
 }
